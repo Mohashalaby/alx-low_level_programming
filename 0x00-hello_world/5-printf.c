e49da5379765b52f610cf3bf9d086c90314e2bbf
mohamed <mohamedshalaby2@aol.com>
2023-09-07 17:24:24 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+ * this documintions 
+ * what the code do 
+ * this is entry point 
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art.\n");
