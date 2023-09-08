dcd57f440e6129e23c9a4efced97557ce9882df8
mohamed <mohamedshalaby2@aol.com>
2023-09-08 12:27:57 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+*main - entry point
+*
+* Description: printing alphapet
+*
+* Return: 0(success)
+*
+*/
+
+int main(void)
+{
+	for (int i = 97; i < 123; i++)
+	{
+		putchar(i);
+}
+	for (int i = 65; i < 91; i++)
+{
+		putchar (i);
+}
+
+	return (0);
+}
