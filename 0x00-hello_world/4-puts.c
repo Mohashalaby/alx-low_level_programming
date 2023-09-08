eef77e8eee96585b29787c0a996e1175efa2a2a5
mohamed <mohamedshalaby2@aol.com>
2023-09-08 07:32:52 -0700

    Puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 /**
- * this describe 
+ * this describe
  *
  * how to print with puts function
  *
  * using return value 0
  *
- * */
+ **/
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
-	return(0);
+	return (puts("\"Programming is like building a multilingual puzzle\n"));
 }
-	
+
