faf9e1bc50fc7a1804528b7766e4dcd5b60e98e7
mohamed <mohamedshalaby2@aol.com>
2023-09-10 12:16:18 -0700

    mmm

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,7 +14,7 @@ int main(void)
 	printf("Size of an int:%u byte(s)\n", sizeof(int));
 	printf("Size of a long int:%u byte(s)\n", sizeof(long int));
 	printf("Size of a long long int:%u byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %u byte(s)", sizeof(float));
+	printf("Size of a float: %u byte(s)\n", sizeof(float));
 	return (0);
 }
 
