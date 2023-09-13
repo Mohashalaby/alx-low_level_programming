264d4a077eecfc994c592e746ffdb7da4803fe2b
mohamed <mohamedshalaby2@aol.com>
2023-09-13 15:14:08 -0700

    mmm

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _islower  - the function to print all alphabet from a-z 10x
+ * _islower  - the function check if the charachter is alower
  *Return: return 1 if c is alowercase otherwise 0 success
  *@c:  to check the input of the method
 **/
@@ -10,5 +10,5 @@ int _islower(int c);
 {
 	if (c >= 92 && c <= 122)
 		return (1);
-	return (0);
+                return (0);
 }
