fc1bd6e9177b4111869472046d659207fdb0a0da
mohamed <mohamedshalaby2@aol.com>
2023-09-17 00:52:46 -0700

    nbn

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _print numbers - to print numbes
+ **/
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+
+	_putchar(i);
+	_putchar('\n');
+	return (0);
+}
+
+
