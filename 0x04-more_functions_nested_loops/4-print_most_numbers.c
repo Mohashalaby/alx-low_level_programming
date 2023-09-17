f67b066fb30e237338ca4082811db5e72bc9bbb1
mohamed <mohamedshalaby2@aol.com>
2023-09-17 02:10:58 -0700

    jhj

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_most_numbers - print numbers form 1 to 9 except 2&4
+ * Return: always 0
+ **/
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		if (n == 2 || n == 4)
+			continue;
+		_putchar(n + 48);
+	}
+	_putchar('\n');
