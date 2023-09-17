54b18434915eb1ae8dcf95221ba482ce0efec9b9
mohamed <mohamedshalaby2@aol.com>
2023-09-17 02:35:15 -0700

    dsd

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -5,11 +5,11 @@
  **/
 void print_most_numbers(void)
 {
-	int n;
+	char n;
 
-	for (n = 0; n <= 9; n++)
+	for (n = '0'; n <= '9'; n++)
 	{
-		if (!(n == 2 || n == 4);
-		_putchar(n + 48);
+		if (!(n == '2' || n == '4');
+		_putchar(n);
 	}
 	_putchar('\n');
