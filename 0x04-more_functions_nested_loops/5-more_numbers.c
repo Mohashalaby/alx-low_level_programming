14d6276beab7c6a821b850776fe123c3c6455dfb
mohamed <mohamedshalaby2@aol.com>
2023-09-17 12:19:29 -0700

    alx3

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * more_numbers - print more numbers in rows
+ *                         an example 1-14 10 times
+ *
+ * Return: when return 0 always success
+ */
+void more_numbers(void)
+{
+int num1, num2, sum;
+
+	for (num2 = 0; num2 <= 10; num2++)
+	{
+		for (sum = 0; sum <= 14; sum++)
+		{
+			num1 = sum;
+			if (sum > 9)
+			{
+				_putchar(1 + 48);
+				num1 = sum % 10;
+			}
+			_putchar(num1 + 48);
+		}
+		_putcchar('\n');
+
+
+
+
+	}
