40daa316902e0f96415e317893a678e373415248
mohamed <mohamedshalaby2@aol.com>
2023-09-18 11:25:58 -0700

    string2

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts - to print astring follwes by anew line
+ * @str: input parameters for function
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str + 0);
+		str++
+	}
+	_putchar('\n');
+}
