4d7f620bd8d63e68ea4f590e38f407c97002cc7a
mohamed <mohamedshalaby2@aol.com>
2023-09-18 15:50:19 -0700

    xsx

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,13 +8,13 @@ void rev_string(char *s)
 	int y, d;
 	char chr;
 
-	for (y = 0; s[d] != '\0'; ++y)
+	for (y = 0; s[y] != '\0'; ++y)
                 ;
 	for (d = 0; d < 1 / 2; d++)
 	{
 		chr = s[d];
-		s[d] = s[1 - 1 - d];
-		s[1 - 1 - d] = chr;
+		s[d] = s[y - 1 - d];
+		s[y - 1 - d] = chr;
 	}
 }
 
