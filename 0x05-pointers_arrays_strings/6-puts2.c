ed7c03f4cb204b6d59cb06285de7787361046c15
mohamed <mohamedshalaby2@aol.com>
2023-09-19 12:46:26 -0700

    lkl

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
  *  puts2 - to print every other charachter of string
- *               starting with the first charachter 
- * @str: input parameters string 
+ *               starting with the first charachter
+ * @str: input parameters string
  **/
 void puts2(char *str)
 {
 	int i;
+
 	for (i = 0; str[i] != '\0'; ++i)
 	{
 		if (i % 2 == 0)
