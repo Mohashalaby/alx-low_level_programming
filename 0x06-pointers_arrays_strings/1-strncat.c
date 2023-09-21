2938cb50b52698f2a9d384ca77d264c277199e6f
mohamed <mohamedshalaby2@aol.com>
2023-09-21 12:52:57 -0700

    concat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * __strncat - to concatonaties two strings
+ * @dest: pointer to destnation input
+ * @src: pointer to source
+ * @n: numbers of bytes
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int a = 0;
+	int b;
+
+	while (dest[a])
+		a++;
+	for (b = 0; b < n && src[b] != '\n'; b++)
+		dest[a + b] = src[b];
+	dest[a + b] = '\0';
+	return (dest);
+}
