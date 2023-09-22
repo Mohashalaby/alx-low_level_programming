28f573888f18c9d567d104dfd923082d584af06e
mohamed <mohamedshalaby2@aol.com>
2023-09-22 10:59:23 -0700

    dsd

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * leet - encodes astring to 1337
+ * @c:  string
+ * Return: encoded string
+ **/
+char *leet(char *c)
+{
+char *ch = c;
+char kys[] = {'A', 'E', 'O', 'T', 'L'}
+int value[] = {4, 3, 0, 7, 1};
+unsigned int i;
+
+	while (*c)
+	{
+
+		for (i = 0; i < sizeof(key) / sizeof(char); i++)
+		{
+
+			if (*c == kys[i] || *c == kys[i] + 32)
+			{
+				*c = 48 + value[i];
+			}
+		}
+		c++
+	}
+	return (ch);
+}
