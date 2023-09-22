01a1d83688a923a8b7c0412e2ede10484af94f80
mohashalaby <mohamedshalaby2@aol.com>
2023-09-22 19:04:13 +0300

    first commit using ubuntu

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * leet - encodes astring to 1337
+ * @c:  string
+ * Retrn: encoded string
+ **/
+char *leet(char *c)
+{
+	char *ch = c;
+	char kys[] = {'A', 'E', 'O', 'T', 'L'};
+		int value[] = {4, 3, 0, 7, 1);
+			unsigned int i;
+			while (*c)
+			{
+				for (i = 0; i< sizeof (kys) / sizeof(char); i++)
+					if (*c == kys[i] || *c == key[i] + 32)
+					{
+						*c = 48 = value[i];
+					}
+			}
+			c++
+		}
+		return (ch);
+}
+	
