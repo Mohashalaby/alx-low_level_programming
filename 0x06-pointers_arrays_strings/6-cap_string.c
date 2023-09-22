80bc48310602df54bd64734c24610719c7dd0eda
mohamed <mohamedshalaby2@aol.com>
2023-09-22 04:50:53 -0700

    capital

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * *cap_string - that capitalizes all words of a string.
+ * @c: input string
+ * Return: string with capitalized
+ **/
+char *cap_string(char *c)
+{
+	char *ptr = c;
+	int fDlimit = 1;
+
+	while (*c)
+	{
+		if (isDelimiter(*c))
+			fDlimit = 1;
+		else if (isLower(*c) && fDlimit)
+		{
+			*c = 32;
+			fDlimit = 0;
+		}
+		else
+			fDlimit = 0;
+		c++;
+	}
+	return (*ptr);
+}
