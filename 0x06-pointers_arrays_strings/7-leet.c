7ee82b2ff1c434ec677be82d4f6b15483bffd2d7
mohamed <mohamedshalaby2@aol.com>
2023-09-22 11:08:14 -0700

    sds

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -27,27 +27,3 @@ unsigned int i;
 	}
 	return (ch);
 }
-=======
- * Retrn: encoded string
- **/
-char *leet(char *c)
-{
-	char *ch = c;
-	char kys[] = {'A', 'E', 'O', 'T', 'L'};
-		int value[] = {4, 3, 0, 7, 1};
-			unsigned int i;
-
-			while (*c)
-			{
-				for (i = 0; i< sizeof (kys) / sizeof(char); i++)
-					if (*c == kys[i] || *c == kys[i] + 32)
-					{
-						*c = 48 + value[i];
-					}
-			}
-			c++
-		}
-		return (ch);
-}
-	
->>>>>>> f11ddedcf0594fe4abb83d646cdc65ea80c4ceb8
