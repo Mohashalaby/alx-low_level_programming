59b4c187f657bce4ec36506520f7f9abc961229c
mohamed <mohamedshalaby2@aol.com>
2023-09-22 03:02:27 -0700

    convert

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/*
+* *string_toupper - changes all lowercase letters of a string to upper
+* @str: string to return
+* Return : str
+**/
+char *string_toupper(char *str)
+{
+	int x;
+
+	for (x = 0; str[x] != '\0'; x++)
+	{
+		if (str[x] >= 97 && str[x] <= 122)
+		{
+			str[x] = str[x] - 32;
+		}
+	}
+	return (str);
+}
+
