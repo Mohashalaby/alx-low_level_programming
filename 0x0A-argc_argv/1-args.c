f2a682ba717d265d267a6a25bd05c856cf0e351d
mohamed <mohamedshalaby2@aol.com>
2023-10-06 04:42:24 -0700

    nargs

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ *
+ * main - print number of arguments
+ * @argc: number of arguments
+ * @argv: number of arrays
+ * Return: return 0 success else fail.
+ **/
+
+int main(int argc, char *argv[]__attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
+
