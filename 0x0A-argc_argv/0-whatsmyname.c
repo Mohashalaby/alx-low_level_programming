fa22d1143d6387eb6497657f04adf5cdc2e45b30
mohamed <mohamedshalaby2@aol.com>
2023-10-06 04:05:40 -0700

    argc

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point , prints aname of program
+ * @argc: arguments
+ * @argv: arrays
+ * Return: return 0 success
+ **/
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
