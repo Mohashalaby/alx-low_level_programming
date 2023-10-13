3ceb9f991b36e70bbeed6fb1a1039054caed8f5a
mohamed <mohamedshalaby2@aol.com>
2023-10-13 08:07:08 -0700

    3m

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,4 +1,6 @@
 #include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
 
 /**
  * format_char - format char
@@ -35,7 +37,7 @@ void format_float(char *separator, va_list ap)
  **/
 void format_string(char *separator, va_list ap)
 {
-	char *str = va_arg(ap, char *)
+	char *str = va_arg(ap, char *);
 
 		switch ((int)(!str))
 	case 1:
