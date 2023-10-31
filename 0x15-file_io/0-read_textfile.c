e23941c6a503e1603cc5b5503cc166988bab47fd
mohamed <mimoo888@gmail.com>
2023-10-31 12:51:06 -0700

    0io

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * read_textfile - read atext from afile and print it
+ * @filename: name of file
+ * @letters: numbers of bytes
+ * Return: number of bytes
+ **/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t bytes;
+	char buf[READ_BUF_SIZE * 8];
+
+	if (!filename || letters)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	bytes = read(fd, &buf[0], letters);
+	bytes = write(STDOUT_FILENO, &buf[0], bytes);
+	close(fd);
+	return (bytes);
+}
